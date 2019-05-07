eaa0c8507cee5ee79fa90b19da878d932d092db5
Максим Макаров <max2@Maksims-MacBook-Pro.local>
2019-05-07 09:12:45 +0300

    fixed logs

--- methods/uploadNginxConfig.js
+++ methods/uploadNginxConfig.js
@@ -3,9 +3,14 @@ const path = require('path')
 
 module.exports = async function uploadNginxConfig(conn, filename) {
     const sftp = await Promise.promisify(conn.sftp, { multiArgs: false, context: conn })()
-    const res = await Promise.promisify(sftp.fastPut, { multiArgs: false, context: sftp })(
+    await Promise.promisify(sftp.fastPut, { multiArgs: false, context: sftp })(
         filename,
         path.join('/etc/nginx/sites-enabled', path.basename(filename))
     )
+    try {
+        await Promise.promisify(conn.exec, { multiArgs: false, context: conn })('service nginx restart')
+    } catch (err) {
+        console.log(`Can't restart nginx. You need to restart it manually. `, err, )
+    }
     return res
 }
